685c1d70347105ffe9257f573298315a9fe27f7f
Lucas <lucasalvlacerda@gmail.com>
2018-12-05 02:06:21 -0200

    Adiciona animaçoes ao fazer scroll; adiciona conteudo na section de quem sao; adiciona conteudo no contato; finaliza o footer com os logos utilizando fontawesome

---
+++ js/home.js
@@ -0,0 +1,16 @@
+const quemsaoSection = document.querySelector('#quemsao');
+const contactSection = document.querySelector("#contact");
+const quemsaoDescriptionContent = document.querySelector("#description-content");
+const contactBirds = document.querySelector("#contact-bird-image>img");
+
+function showContent() {
+
+    if (window.pageYOffset >= (quemsaoSection.offsetHeight - 300)) {
+      quemsaoDescriptionContent.classList.add('show-content');
+    } 
+    
+    if (window.pageYOffset >= (contactSection.offsetHeight + 150)) {
+        console.log('do something');
+        contactBirds.classList.add("show-img");
+    }
+}
